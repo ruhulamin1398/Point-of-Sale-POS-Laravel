1364935b7b16b81b3e226ef4660a251d717f34fc
ruhulaminrul <ruhul.ok@gmail.com>
2020-09-23 16:51:55 +0600

    add sb admin 2

---
+++ Delta/resources/views/includes/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abasas POS</title>
+
+    <link rel="stylesheet" href="{{asset('css/fontawesome-free/css/all.min.css')}}" type="text/css">
+    
+    <link rel="stylesheet" href="{{asset('css/admin/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin/sb-admin-2.min.css')}}">
+
+</head>
+<body>
+    
+@yield('content')
+
+
+
+<script src="{{asset('js/admin/jquery.min.js')}}"></script>
+<script src="{{asset('js/admin/bootstrapbundle.js')}}"></script>
+<script src="{{asset('js/admin/easing.min.js')}}"></script>
+<script src="{{asset('js/admin/sb-admin-2.min.js')}}"></script>
+<script src="{{asset('js/admin/Chart.min.js')}}"></script>
+
+</body>
+</html>
--- Delta/webpack.mix.js
+++ Delta/webpack.mix.js
@@ -18,4 +18,4 @@ mix.js('resources/js/app.js', 'public/js')
     ]);
 
 
-    
+        
