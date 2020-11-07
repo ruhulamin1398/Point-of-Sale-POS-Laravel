f9bb3e431c16d17851b197e7df9f2eb97ae0d86f
masumbillah3416 <masumbillahsujon1998@gmail.com>
2020-11-07 17:58:40 +0600

    analysis

--- Delta/app/Http/Controllers/AnalysisController.php
+++ Delta/app/Http/Controllers/AnalysisController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\calculationAnalysisDaily;
+use App\Models\calculationAnalysisMonthly;
 use App\Models\sellAnalysisDaily;
 use App\Models\sellAnalysisMonthly;
 use Carbon\Carbon;
@@ -11,96 +13,189 @@ class AnalysisController extends Controller
 {
     public function index()
     {
-        $dates= array();
-        $months= array();
-        $DailySell= array();
-        $monthlySell= array();
 
-        $monthStart = Carbon:: now()->format('Y-m-01');
-        $monthEnd = Carbon:: now()->format('Y-m-31');
-        $yearStart = Carbon:: now()->format('Y-01-01');
-        $yearEnd = Carbon:: now()->format('Y-12-31');
+        $sellAnalysisDaily=$this->sellAnalysisDaily();
+        $sellAnalysisMonthly=$this->sellAnalysisMonthly();
+        $amountAnalysisDaily= $this->amountAnalysisDaily();
+        $amountAnalysisMonthly= $this->amountAnalysisMonthly();
 
-        $sellDailys = sellAnalysisDaily::where('date','>=',$monthStart)->where('date','<=',$monthEnd)->get();
-        $sellmonthlys = sellAnalysisMonthly::where('month','>=',$yearStart)->where('month','<=',$yearEnd)->get();
+        $sellAnalysisDaily = json_decode(json_encode($sellAnalysisDaily), true);
+        $sellAnalysisMonthly = json_decode(json_encode($sellAnalysisMonthly), true);
+        $amountAnalysisDaily = json_decode(json_encode($amountAnalysisDaily), true);
+        $amountAnalysisMonthly = json_decode(json_encode($amountAnalysisMonthly), true);
 
-        foreach($sellDailys as $sellDaily){
-            $date = Carbon::parse($sellDaily->date)->format('d M,Y');
-            array_push($dates,$date);
-            array_push($DailySell,$sellDaily->count);
-        }
-        foreach($sellmonthlys as $sellmonthly){
-            $month = Carbon::parse($sellmonthly->month)->format('M,Y');
-            array_push($months,$month);
-            array_push($monthlySell,$sellmonthly->count);
-        }
-        
-
-        $sellAnalysisDaily= [
-            'id'=>'sellAnalysisDaily',
-            'label'=>"Sell Count",
-            "lebels" =>$dates,
-            'data' =>$DailySell,
-            'color'=>'#C70039',
-        ];
-        $sellAnalysisMonthly= [
-            'id'=>'sellAnalysisMonthly',
-            'label'=>"Sell Count",
-            "lebels" =>$months,
-            'data' =>$monthlySell,
-            'color'=>'#FF5733',
-        ];
-
-
-        // $dailyGraphData= [
-        //     'id'=>'dailyGraphData',
-        //     "lebels" =>$dailyLabels,
-        //     "datasets"=> [
-        //         [
-        //             "label" => "Daily Cost",
-        //             "data" => $dailyCost,
-        //             "backgroundColor" => "#FF0000",
-        //             "borderColor" => 	"#FF0000",
-        //             "fill" => false
-        //         ],[
-        //             "label" => "daily Total Amount",
-        //             "data" => $dailyAmount,
-        //             "backgroundColor" => "#008000",
-        //             "borderColor" => 	"#008000",
-        //             "fill" => false
-        //         ],[
-        //             "label" => "Daily Discount",
-        //             "data" => $dailyDiscount,
-        //             "backgroundColor" => "#0000FF",
-        //             "borderColor" => 	"#0000FF",
-        //             "fill" => false
-        //         ],[
-        //             "label" => "Daily Due",
-        //             "data" => $dailyDue,
-        //             "backgroundColor" => "#008080",
-        //             "borderColor" => 	"#008080",
-        //             "fill" => false
-        //         ],[
-        //             "label" => "Daily Cash Recieved",
-        //             "data" => $dailyCash,
-        //             "backgroundColor" => "#00FFFF",
-        //             "borderColor" => 	"#00FFFF",
-        //             "fill" => false
-        //         ],
-        //     ]
-        
-          
-        // ];
-
-
-
-
-
-        $sellAnalysisDaily= json_decode (json_encode($sellAnalysisDaily) , true);
-        $sellAnalysisMonthly= json_decode (json_encode($sellAnalysisMonthly) , true);
-        // $sellAnalysisYearly= json_decode (json_encode($sellAnalysisYearly) , true);
-        // $dailyGraphData= json_decode (json_encode($dailyGraphData) , true);
-
-        return view('analysis.index',compact('sellAnalysisDaily','sellAnalysisMonthly'));
+        return view('analysis.index', compact('sellAnalysisDaily', 'sellAnalysisMonthly','amountAnalysisDaily','amountAnalysisMonthly'));
     }
+
+
+
+
+
+
+// ***************** sellAnalysisDaily ******************
+
+    public function sellAnalysisDaily(){
+        $sellDates = array();
+        $dailySell = array();
+        $monthStart = Carbon::now()->format('Y-m-01');
+        $monthEnd = Carbon::now()->format('Y-m-31');
+        $sellDailys = sellAnalysisDaily::where('date', '>=', $monthStart)->where('date', '<=', $monthEnd)->get();
+        foreach ($sellDailys as $sellDaily) {
+            $date = Carbon::parse($sellDaily->date)->format('d M,Y');
+            array_push($sellDates, $date);
+            array_push($dailySell, $sellDaily->count);
+        }
+        $sellAnalysisDaily = [
+            'id' => 'sellAnalysisDaily',
+            "lebels" => $sellDates,
+            "datasets" => [
+                [
+                    "label" => "Daily Sell",
+                    "data" => $dailySell,
+                    "backgroundColor" => "#C70039",
+                    "borderColor" =>     "#C70039",
+                    "fill" => false
+                ],
+            ]
+        ];
+        return $sellAnalysisDaily;
+    }
+
+// ***************** sellAnalysisMonthly ******************
+
+    public function sellAnalysisMonthly(){
+        $sellMonths = array();
+        $monthlySell = array();
+        $yearStart = Carbon::now()->format('Y-01-01');
+        $yearEnd = Carbon::now()->format('Y-12-31');
+        $sellMonthlys = sellAnalysisMonthly::where('month', '>=', $yearStart)->where('month', '<=', $yearEnd)->get();
+        foreach ($sellMonthlys as $sellMonthly) {
+            $month = Carbon::parse($sellMonthly->month)->format('M,Y');
+            array_push($sellMonths, $month);
+            array_push($monthlySell, $sellMonthly->count);
+        }
+        $sellAnalysisMonthly = [
+            'id' => 'sellAnalysisMonthly',
+            "lebels" => $sellMonths,
+            "datasets" => [
+                [
+                    "label" => "Monthly Sell",
+                    "data" => $monthlySell,
+                    "backgroundColor" => "#C70039",
+                    "borderColor" =>     "#C70039",
+                    "fill" => false
+                ],
+            ]
+        ];
+        return $sellAnalysisMonthly;
+    }
+
+// ***************** amountAnalysisDaily ******************
+
+    public function amountAnalysisDaily(){
+
+        $amountDates = array();
+        $dailySellAmount = array();
+        $dailyBuyAmount = array();
+        $dailyProfitAmount = array();
+        $monthStart = Carbon::now()->format('Y-m-01');
+        $monthEnd = Carbon::now()->format('Y-m-31');
+        $amountDailys = calculationAnalysisDaily::where('date', '>=', $monthStart)->where('date', '<=', $monthEnd)->get();
+
+        foreach ($amountDailys as $amountDaily) {
+            $date = Carbon::parse($amountDaily->date)->format('d M,Y');
+            array_push($amountDates, $date);
+            array_push($dailySellAmount, $amountDaily->sell);
+            array_push($dailyBuyAmount, $amountDaily->buy);
+            array_push($dailyProfitAmount, $amountDaily->sell_profit);
+        }
+        $amountAnalysisDaily = [
+            'id' => 'amountAnalysisDaily',
+            "lebels" => $amountDates,
+            "datasets" => [
+                [
+                    "label" => "Daily Buy",
+                    "data" => $dailyBuyAmount,
+                    "backgroundColor" => "#FF0000",
+                    "borderColor" =>     "#FF0000",
+                    "fill" => false
+                    
+                ],
+                [
+                    "label" => "Daily Sell",
+                    "data" => $dailySellAmount,
+                    "backgroundColor" => "#008000",
+                    "borderColor" =>     "#008000",
+                    "fill" => false
+                ],
+                [
+                    "label" => "Daily Sell Profit",
+                    "data" => $dailyProfitAmount,
+                    "backgroundColor" => "#0000FF",
+                    "borderColor" =>     "#0000FF",
+                    "fill" => false
+                ],
+            ]
+        ];
+        return $amountAnalysisDaily;
+
+    }
+
+// ***************** amountAnalysisMonthly ******************
+
+    public function amountAnalysisMonthly(){
+
+        $amountMonths = array();
+        $monthlySellAmount = array();
+        $monthlyBuyAmount = array();
+        $monthlyProfitAmount = array();
+
+        $yearStart = Carbon::now()->format('Y-01-01');
+        $yearEnd = Carbon::now()->format('Y-12-31');
+
+        $amountMonthlys = calculationAnalysisMonthly::where('month', '>=', $yearStart)->where('month', '<=', $yearEnd)->get();
+
+        
+        foreach ($amountMonthlys as $amountMonthly) {
+            $month = Carbon::parse($amountMonthly->month)->format('M,Y');
+            array_push($amountMonths, $month);
+            array_push($monthlySellAmount, $amountMonthly->sell);
+            array_push($monthlyBuyAmount, $amountMonthly->buy);
+            array_push($monthlyProfitAmount, $amountMonthly->sell_profit);
+        }
+
+
+        $amountAnalysisMonthly = [
+            'id' => 'amountAnalysisMonthly',
+            "lebels" => $amountMonths,
+            "datasets" => [
+                [
+                    "label" => "Monthly Buy",
+                    "data" => $monthlyBuyAmount,
+                    "backgroundColor" => "#FF0000",
+                    "borderColor" =>     "#FF0000",
+                    "fill" => false
+                ],
+                [
+                    "label" => "Monthly Sell",
+                    "data" => $monthlySellAmount,
+                    "backgroundColor" => "#008000",
+                    "borderColor" =>     "#008000",
+                    "fill" => false
+                ],
+                [
+                    "label" => "Monthly Sell Profit",
+                    "data" => $monthlyProfitAmount,
+                    "backgroundColor" => "#0000FF",
+                    "borderColor" =>     "#0000FF",
+                    "fill" => false
+                ],
+            ]
+        ];
+        return $amountAnalysisMonthly;
+    }
+
+
+
+
 }
--- Delta/resources/views/analysis/index.blade.php
+++ Delta/resources/views/analysis/index.blade.php
@@ -9,6 +9,10 @@
 <x-bar-chart :dataArray="$sellAnalysisDaily" />
 <h1 style="text-align: center">Sell Monthly</h1> 
 <x-bar-chart :dataArray="$sellAnalysisMonthly" />
+<h1 style="text-align: center">Amount Monthly</h1> 
+<x-bar-chart :dataArray="$amountAnalysisMonthly" />
+<h1 style="text-align: center">Amount Daily</h1> 
+<x-bar-chart :dataArray="$amountAnalysisDaily" />
 
  
 @endsection
