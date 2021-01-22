7c895837e5bce0edb3013a96fec3c6ef39c0fd4d
ruhulaminrul <ruhul.ok@gmail.com>
2021-01-22 11:54:30 +0600

    data sync

--- Delta/app/Console/Kernel.php
+++ Delta/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\datasync;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        datasync::class,
     ];
 
     /**
@@ -25,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+         $schedule->command('data:sync')->everyMinute();
     }
 
     /**
