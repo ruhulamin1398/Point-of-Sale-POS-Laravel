208655e7cefc817049670be90cd138892f384c8a
masumbillah3416 <masumbillahsujon1998@gmail.com>
2021-01-24 17:46:46 +0600

    sync message

--- Delta/routes/web.php
+++ Delta/routes/web.php
@@ -209,6 +209,21 @@ Route::get("chart", function () {
 
 
 Route::get('sync-test', function () {
+    
+    
+    
+  
+    $connected = @fsockopen("www.example.com", 80); 
+                                        //website, port  (try 80 or 443)
+    if ($connected){
+        $is_conn = 'true'; //action when connected
+        fclose($connected);
+    }else{
+        $is_conn = 'false'; //action in connection failure
+    }
+    return $is_conn;
+
+
 
     // $datas = onlineSync::all();
     // foreach ($datas as $data) {
