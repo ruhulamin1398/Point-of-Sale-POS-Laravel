1d3fb5fbbe83cf2c86db0431253cbe935cf2f58c
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-25 09:12:04 +0600

    soft delete

--- Delta/app/Models/expenseMonthly.php
+++ Delta/app/Models/expenseMonthly.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class expenseMonthly extends Model
 {
-    use HasFactory;    
+    use HasFactory;  
+    use SoftDeletes;  
     protected $guarded = [];
 
     public function employee(){
