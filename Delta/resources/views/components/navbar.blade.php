f82b45e55680d192e6f7889f262a32b0a1c6e350
sourovahmad <mdsourovahmad19@gmail.com>
2021-01-28 13:14:18 +0600

    issue

--- Delta/resources/views/components/navbar.blade.php
+++ Delta/resources/views/components/navbar.blade.php
@@ -88,9 +88,9 @@
             <!-- Nav Item - Messages -->
             <li class="nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <!-- <i class="fas fa-envelope fa-fw"></i> -->
-                <!-- Counter - Messages -->
-                <!-- <span class="badge badge-danger badge-counter">7</span> -->
+      <i class="fas fa-envelope "></i> 
+                 
+               <span class="badge badge-danger badge-counter">7</span>
               </a>
               <!-- Dropdown - Messages -->
               <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
