55da8d746a012a02d90c1372d6f4e22edbbe63c4
masumbillah3416 <masumbillahsujon1998@gmail.com>
2021-01-28 13:52:55 +0600

    logo and sidebar

--- Delta/resources/views/auth/login.blade.php
+++ Delta/resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            {{-- <x-jet-authentication-card-logo /> --}}
+            <img src="{{asset('image/abasas-logo.png')}}" width="200px" alt="Logo">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
