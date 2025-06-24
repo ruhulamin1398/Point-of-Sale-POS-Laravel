806dbd5ea3e9d48bbc0726e5239c0b190ff286b1
ruhulamin1398 <ruhul.ok@gmail.com>
2025-06-24 12:47:06 +0600

    updated to laravel 10

--- Delta/app/Http/Middleware/TrustProxies.php
+++ Delta/app/Http/Middleware/TrustProxies.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
@@ -19,5 +19,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        SymfonyRequest::HEADER_X_FORWARDED_FOR
+        | SymfonyRequest::HEADER_X_FORWARDED_HOST
+        | SymfonyRequest::HEADER_X_FORWARDED_PORT
+        | SymfonyRequest::HEADER_X_FORWARDED_PROTO
+        | SymfonyRequest::HEADER_X_FORWARDED_PREFIX;
 }
